b25852ff99f28ae8bf1bbf098180a07a2f1cf79c
Kelly Norton <kellegous@gmail.com>
2023-05-19 16:41:05 -0400

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,53 @@
-use clap::Parser;
-use std::collections::{HashMap, VecDeque};
+use clap::{Parser, ValueEnum};
 use std::io::prelude::*;
-use std::path::Iter;
 use std::{error::Error, io};
 
 #[derive(Debug, Clone)]
 struct Puzzle {
     target: u32,
-    values: Vec<u32>,
+    digits: Vec<u32>,
 }
 
 impl Puzzle {
     fn from_arg(s: &str) -> Result<Self, String> {
         s.parse::<Self>().map_err(|e| e.to_string())
+    }
+
+    fn all_solutions(&self) -> Vec<Vec<BinOp>> {
+        fn collect_solutions(
+            digits: &[u32],
+            target: u32,
+            steps: &[BinOp],
+            solutions: &mut Vec<Vec<BinOp>>,
+        ) {
+            for (a, b) in all_pairs_in(digits) {
+                let mut rest = digits.to_vec();
+                rest.remove(rest.iter().position(|&v| v == a).unwrap());
+                rest.remove(rest.iter().position(|&v| v == b).unwrap());
+
+                for op in BinOp::all_for(a, b) {
+                    let result = match op.evaluate() {
+                        Some(v) => v,
+                        None => continue,
+                    };
+
+                    let mut steps = steps.to_vec();
+                    steps.push(op);
+
+                    rest.push(result);
+                    collect_solutions(&rest, target, &steps, solutions);
+                    rest.pop();
+
+                    if result == target {
+                        solutions.push(steps);
+                    }
+                }
+            }
+        }
+
+        let mut solutions = Vec::new();
+        collect_solutions(&self.digits, self.target, &[], &mut solutions);
+        solutions
     }
 }
 
@@ -22,19 +57,19 @@ impl std::str::FromStr for Puzzle {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let ia = s.find('=').ok_or("missing =")?;
         let target = s[ia + 1..].trim().parse::<u32>()?;
-        let values = s[..ia]
+        let digits = s[..ia]
             .split(',')
             .map(|s| s.trim().parse::<u32>())
             .collect::<Result<Vec<_>, _>>()?;
-        if values.len() != 6 {
+        if digits.len() != 6 {
             return Err("expected 6 values".into());
         }
 
-        Ok(Self { target, values })
+        Ok(Self { target, digits })
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Op {
     Add,
     Sub,
@@ -69,7 +104,7 @@ impl BinOp {
         Self { op, l, r }
     }
 
-    fn apply(&self) -> Option<u32> {
+    fn evaluate(&self) -> Option<u32> {
         let l = self.l;
         let r = self.r;
         match self.op {
@@ -110,61 +145,6 @@ impl std::fmt::Display for BinOp {
     }
 }
 
-struct Solutions {
-    target: u32,
-    queue: VecDeque<(Vec<BinOp>, Vec<u32>)>,
-}
-
-impl Solutions {
-    fn from_puzzle(p: &Puzzle) -> Self {
-        let mut queue = VecDeque::new();
-        queue.push_back((vec![], p.values.clone()));
-        Self {
-            target: p.target,
-            queue,
-        }
-    }
-}
-
-impl Iterator for Solutions {
-    type Item = Vec<BinOp>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            let (ops, digits) = match self.queue.pop_front() {
-                Some(v) => v,
-                None => return None,
-            };
-
-            for (a, b) in all_pairs_in(&digits) {
-                let mut rest = digits.to_vec();
-                rest.remove(rest.iter().position(|&v| v == a).unwrap());
-                rest.remove(rest.iter().position(|&v| v == b).unwrap());
-                for op in BinOp::all_for(a, b) {
-                    let result = match op.apply() {
-                        Some(v) => v,
-                        None => continue,
-                    };
-
-                    let mut rest = rest.to_vec();
-                    rest.push(result);
-
-                    let mut ops = ops.to_vec();
-                    ops.push(op);
-
-                    self.queue.push_back((ops, rest));
-                }
-            }
-
-            if let Some(result) = ops.last().and_then(|op| op.apply()) {
-                if result == self.target {
-                    return Some(ops);
-                }
-            }
-        }
-    }
-}
-
 fn all_pairs_in(values: &[u32]) -> impl Iterator<Item = (u32, u32)> + '_ {
     values
         .iter()
@@ -172,117 +152,62 @@ fn all_pairs_in(values: &[u32]) -> impl Iterator<Item = (u32, u32)> + '_ {
         .flat_map(|(i, &v)| values[i + 1..].iter().map(move |&w| (v, w)))
 }
 
-fn replace_op_in(values: &[u32], op: &BinOp, res: u32) -> Vec<u32> {
-    let mut values = values.to_vec();
-    let i = values.iter().position(|&v| v == op.l).unwrap();
-    let j = values.iter().position(|&v| v == op.r).unwrap();
-    values[i] = res;
-    values.remove(j);
-    values
+#[derive(Debug, ValueEnum, Clone, Copy)]
+enum Strategy {
+    Shortest,
+    Longest,
+    MostDivides,
 }
 
-fn collect_solutions(
-    values: &[u32],
-    target: u32,
-    steps: &[BinOp],
-    solutions: &mut Vec<Vec<BinOp>>,
-) {
-    for (a, b) in all_pairs_in(values) {
-        let mut rest = values.to_vec();
-        rest.remove(rest.iter().position(|&v| v == a).unwrap());
-        rest.remove(rest.iter().position(|&v| v == b).unwrap());
-
-        for op in BinOp::all_for(a, b) {
-            let result = match op.apply() {
-                Some(v) => v,
-                None => continue,
-            };
-
-            let mut steps = steps.to_vec();
-            steps.push(op);
-
-            rest.push(result);
-            collect_solutions(&rest, target, &steps, solutions);
-            rest.pop();
-
-            if result == target {
-                solutions.push(steps);
+impl Strategy {
+    fn is_better(&self, a: &[BinOp], b: &[BinOp]) -> bool {
+        match self {
+            Self::Shortest => a.len() > b.len(),
+            Self::Longest => a.len() < b.len(),
+            Self::MostDivides => {
+                let a = a.iter().filter(|op| op.op == Op::Div).count();
+                let b = b.iter().filter(|op| op.op == Op::Div).count();
+                a < b
             }
         }
     }
-}
-
-fn solve(values: &[u32], target: u32) -> Vec<BinOp> {
-    let mut steps = Vec::new();
-    for (a, b) in all_pairs_in(values) {
-        for op in BinOp::all_for(a, b) {
-            let result = match op.apply() {
-                Some(v) => v,
-                None => continue,
-            };
-
-            if result == target {
-                return vec![op];
-            }
-
-            let soln = solve(&replace_op_in(values, &op, result), target);
-            if !soln.is_empty() && (steps.is_empty() || soln.len() < steps.len()) {
-                steps.clear();
-                steps.push(op);
-                steps.extend(soln);
-            }
-        }
-    }
-    steps
 }
 
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(value_parser= Puzzle::from_arg)]
     puzzle: Option<Puzzle>,
+
+    #[arg(long, value_enum, default_value_t = Strategy::Shortest)]
+    strategy: Strategy,
 }
 
+fn report(solutions: &[Vec<BinOp>], strategy: Strategy) -> Result<(), Box<dyn Error>> {
+    let best = solutions
+        .iter()
+        .fold(None, |best, soln| match best {
+            Some(best) => {
+                if strategy.is_better(best, soln) {
+                    Some(soln)
+                } else {
+                    Some(best)
+                }
+            }
+            None => Some(soln),
+        })
+        .ok_or("no solution found")?;
+
+    for step in best {
+        println!("{} = {:3}", step, step.evaluate().unwrap());
+    }
+
+    Ok(())
+}
 fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
+
     if let Some(puzzle) = args.puzzle {
-        for step in solve(&puzzle.values, puzzle.target) {
-            println!("{} = {:3}", step, step.apply().unwrap());
-        }
-
-        println!("------");
-
-        let mut stats = HashMap::new();
-
-        // let mut solutions = Vec::new();
-        // collect_solutions(&puzzle.values, puzzle.target, &[], &mut solutions);
-        // for solution in solutions {
-        //     *stats.entry(solution.len()).or_insert(0) += 1;
-        // }
-
-        for solution in Solutions::from_puzzle(&puzzle) {
-            *stats.entry(solution.len()).or_insert(0) += 1;
-        }
-
-        println!("{:?}", stats);
-        // let mut solutions = Vec::new();
-        // collect_solutions(&puzzle.values, puzzle.target, &[], &mut solutions);
-        // for solution in solutions {
-        //     if solution.len() == 5 {
-        //         for step in solution {
-        //             println!("{} = {:3}", step, step.apply().unwrap());
-        //         }
-        //         break;
-        //     }
-        // }
-
-        // for solution in Solutions::from_puzzle(&puzzle) {
-        //     if solution.len() == 5 {
-        //         for step in solution {
-        //             println!("{} = {:3}", step, step.apply().unwrap());
-        //         }
-        //         break;
-        //     }
-        // }
+        report(&puzzle.all_solutions(), args.strategy)?;
     } else {
         let stdin = io::stdin();
         let mut line = String::new();
@@ -302,20 +227,9 @@ fn main() -> Result<(), Box<dyn Error>> {
                 }
             };
 
-            for step in solve(&puzzle.values, puzzle.target) {
-                println!("{} = {:3}", step, step.apply().unwrap());
-            }
-
-            println!("------");
-            let mut solutions = Vec::new();
-            collect_solutions(&puzzle.values, puzzle.target, &[], &mut solutions);
-            for solution in solutions {
-                if solution.len() == 5 {
-                    for step in solution {
-                        println!("{} = {:3}", step, step.apply().unwrap());
-                    }
-                    break;
-                }
+            if let Err(e) = report(&puzzle.all_solutions(), args.strategy) {
+                println!("error: {}", e);
+                continue;
             }
         }
     }
